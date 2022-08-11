ee7ac8e0bcaa02cbd51e3d11d447af3e39560b19
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2022-08-11 14:44:12 +0200

    removed store, implemented exit button (needs bugfix and new image)

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -5,51 +5,8 @@ using UnityEngine.UI;
 
 public class CanvasController : MonoBehaviour
 {
-
-    public GameObject shop;
-    public Button btnShop;
-    public Button btnBack;
-    
-
-    void Start()
-    {
-        btnShop.GetComponent<CanvasRenderer>().SetAlpha(0);
-        //btnShop.onClick.AddListener(() => openShop());
-        btnBack.onClick.AddListener(() => closeShop());
-
-        shop.SetActive(false);
-    }
-
-    private void openShop(){
-        shop.SetActive(true);
-        StartCoroutine(FadeInShop());
-    }
-
-    private void closeShop(){
-        StartCoroutine(FadeOutShop());
-    }
-
-
-    IEnumerator FadeInShop()
-    {
-        shop.GetComponent<CanvasGroup>().alpha = 0f;
-        float fZwischenergebnis = 1;
-        while (shop.GetComponent<CanvasGroup>().alpha < 1)
-        {
-            fZwischenergebnis -= Time.deltaTime * 1;
-            shop.GetComponent<CanvasGroup>().alpha = 1 - fZwischenergebnis;
-            yield return null;
-        }
-    }
-
-    IEnumerator FadeOutShop()
-    {
-        while (shop.GetComponent<CanvasGroup>().alpha > 0)
-        {
-            shop.GetComponent<CanvasGroup>().alpha = shop.GetComponent<CanvasGroup>().alpha - Time.deltaTime * 1;
-            yield return null;
-        }
-
-        shop.SetActive(false);
-    }    
+    public void exitGame(){
+        Debug.Log("exit");
+        Application.Quit();
+    }  
 }
--- Assets/Scripts/FollowFinger.cs
+++ Assets/Scripts/FollowFinger.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class FollowFinger : MonoBehaviour
 {
     private Rigidbody2D rigid;
 
     private Vector3 playerpos;
+
+    private bool canDrag = true;
 
     void Start()
     {
@@ -16,17 +20,56 @@ public class FollowFinger : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (Input.GetMouseButtonDown(0) && mouseIsOverButton(Input.mousePosition)) {
+            canDrag = false;
+        }
+        else if (Input.GetMouseButtonUp(0)) {
+            canDrag = true;
+        }
 
-        if (Input.touchCount > 0)
+        if (Input.touchCount > 0) {
+            if (Input.GetTouch(0).phase == TouchPhase.Began) {
+                if (mouseIsOverButton(Input.GetTouch(0).position)) {
+                    canDrag = false;
+                }
+            }
+        }
+
+        
+
+        if (Input.touchCount > 0 && canDrag)
         {
             playerpos = new Vector3(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).x, Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).y, 0);
         }
-        else if (Input.GetMouseButton(0))
+        else if (Input.GetMouseButton(0) && canDrag)
         {
             playerpos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
         }
 
+        if (Input.touchCount > 0) {
+            if (Input.GetTouch(0).phase == TouchPhase.Ended) {
+                canDrag = true;
+            }
+        }
+
 
         transform.position = playerpos;
+    }
+
+    private bool mouseIsOverButton(Vector3 position)
+    {
+        PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+        pointerEventData.position = position;
+
+        List<RaycastResult> raycastResultList = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerEventData, raycastResultList);
+        for (int i = 0; i < raycastResultList.Count; i++) {
+            if (raycastResultList[i].gameObject.GetComponent<Button>() == null) {
+                raycastResultList.RemoveAt(i);
+                i--;
+            }
+        }
+
+        return raycastResultList.Count > 0;
     }
 }
